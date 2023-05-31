3558b07b131bd782ca36c64f944bc1b041d528d8
test1 <3388962823@qq.com>
2023-05-31 19:09:24 +0800

    main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://v.qq.com/x/cover/mzc0020050ixd7l/s0046io98bk.html")
+}
